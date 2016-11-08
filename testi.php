78ba02dfda2744db321a34f98f811915515c7530
Unknown <pekka.alaluukas@hotmail.com>
2016-11-08 09:12:07 +0200

    Testitiedosto luotu

---
+++ testi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>testisivu</title>
+</head>
+<body>
+
+<h1>Testataan PHP:tä</h1>
+<?php
+$x=5;
+echo 'Muuttujan arvo on '.$x.' testi';
+echo '<br>';
+echo "Muuttujan arvo on $x";
+?>
+
+</body>
+</html>
